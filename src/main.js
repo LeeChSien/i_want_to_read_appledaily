46d80a3dce7948b4c126bd28b7c9e6eb618de6b9
LEE CHIHHSIEN <chsienlee@gmail.com>
2019-04-21 12:07:41 +0900

    Add mock for paywall

--- src/main.js
+++ src/main.js
@@ -1,50 +1,73 @@
-const ElementRepo = {
-  article(doc) {
-    return doc.querySelector(".ndArticle_margin");
-  },
-  articleWrapper(doc) {
-    return doc.querySelector(".ndArticle_content");
-  },
-  blockers(doc) {
-    return doc.querySelectorAll(".ndPaywall,.ndgPayway");
-  }
+const setArticleVisible = () => {
+  const ElementRepo = {
+    article(doc) {
+      return doc.querySelector(".ndArticle_margin");
+    },
+    articleWrapper(doc) {
+      return doc.querySelector(".ndArticle_content");
+    },
+    blockers(doc) {
+      return doc.querySelectorAll(".ndPaywall,.ndgPayway");
+    }
+  };
+  
+  const isDesktop = () => !!document.querySelector(".ndArticle_content");
+  const isMobile = () =>
+    document
+      .querySelector("meta[name=viewport]")
+      .content.includes("width=device-width");
+  
+  ElementRepo.blockers(document).forEach(node =>
+    node.parentNode.removeChild(node)
+  );
+  
+  if (isDesktop()) {
+    fetch(location.href)
+      .then(response => response.text())
+      .then(body => {
+        const parser = new DOMParser();
+        const $article = ElementRepo.article(
+          parser.parseFromString(body, "text/html")
+        );
+        $article.style.removeProperty("display");
+        const $wrapper = ElementRepo.articleWrapper(document);
+        $wrapper.insertBefore($article, $wrapper.firstChild);
+      });
+  } else if (isMobile()) {
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(() => {
+        document.documentElement.style.removeProperty("overflow-y");
+        document.body.style.removeProperty("overflow-y");
+      });
+    });
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["style"]
+    });
+    observer.observe(document.body, {
+      attributes: true,
+      attributeFilter: ["style"]
+    });
+  }  
 };
 
-const isDesktop = () => !!document.querySelector(".ndArticle_content");
-const isMobile = () =>
-  document
-    .querySelector("meta[name=viewport]")
-    .content.includes("width=device-width");
+// Mock for paywall, enable video player.
+(() => {
+  let script = document.createElement('script');
+  code = function() {
+    window.__defineGetter__('paywall', () => () => {
+      console.log('hehehe!');
 
-ElementRepo.blockers(document).forEach(node =>
-  node.parentNode.removeChild(node)
-);
+      $('.ndAritcle_headPic,.ndArticle_margin,.mediabox,#playerVideo,.articulum').show();
+    });
 
-if (isDesktop()) {
-  fetch(location.href)
-    .then(response => response.text())
-    .then(body => {
-      const parser = new DOMParser();
-      const $article = ElementRepo.article(
-        parser.parseFromString(body, "text/html")
-      );
-      $article.style.removeProperty("display");
-      const $wrapper = ElementRepo.articleWrapper(document);
-      $wrapper.insertBefore($article, $wrapper.firstChild);
-    });
-} else if (isMobile()) {
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(() => {
-      document.documentElement.style.removeProperty("overflow-y");
-      document.body.style.removeProperty("overflow-y");
-    });
-  });
-  observer.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ["style"]
-  });
-  observer.observe(document.body, {
-    attributes: true,
-    attributeFilter: ["style"]
-  });
-}
+    window.onload = () => {
+      $(".ads").hide();
+    }
+  }
+  script.textContent = '(' + code + ')()';
+
+  (document.head || document.documentElement).appendChild(script);
+  
+  window.onload = setArticleVisible;
+})();
